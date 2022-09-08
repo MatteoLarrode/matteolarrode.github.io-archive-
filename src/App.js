f5ed4a88297e2f771caa6b6e83cf332a720c0d53
MatteoLarrode <matteo.larrode@orange.fr>
2022-09-07 22:24:09 -0400

    background work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
 import './App.css';
 import profileImage from './img/profile.png';
+import sunrise_bg from './img/sunrise.jpeg';
+import blackTowhite from './img/blacktowhite.png';
+
+import{Parallax, ParallaxLayer} from '@react-spring/parallax';
 
 function App() {
   return (
     <div className="App">
-      <h1>Matteo Larrode</h1>
-      <h2>Welcome to my website</h2>
-      <h3>start scrolling!</h3>
-      <h3>TEEEST</h3>
-      <img src={profileImage} alt="Profile Image" />
+      <Parallax pages={5}>
+
+        {/* background images */}
+        <ParallaxLayer 
+          offset={0}
+          speed={1.2}
+          factor={2}
+          style={{
+            backgroundImage: `url(${sunrise_bg})`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer 
+          offset={2}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${blackTowhite})`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer 
+          offset={0}
+          speed={1}
+          factor={2}
+        />
+
+        <ParallaxLayer offset={0}>
+      
+            <h1>Matteo Larrode</h1>
+            <h1>Welcome to my website</h1>
+            <h3>start scrolling!</h3>
+            <img src={profileImage} alt="Profile" width="400" height= "auto" />
+
+        </ParallaxLayer>
+          
+        <ParallaxLayer offset={1}>
+            <h1>High School</h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2}>
+          <h1>Undergrad: First Year</h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={3}>
+          <h1>Undergrad: Second Year</h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={4}>
+          <h1>Undergrad: Year Abroad</h1>
+        </ParallaxLayer>
+        
+      </Parallax>
     </div>
   );
 }
