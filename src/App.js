8c0dd560fa163a0e13ad2927047143c85f901257
MatteoLarrode <matteo.larrode@orange.fr>
2022-09-09 02:57:35 -0400

    fundamental layers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import profileImage from './img/profile.png';
 import sunrise_bg from './img/sunrise.jpeg';
 import blackTowhite from './img/blacktowhite.png';
@@ -22,7 +21,7 @@ function App() {
         />
 
         <ParallaxLayer 
-          offset={2}
+          offset={1.6}
           speed={1}
           factor={2}
           style={{
@@ -32,18 +31,46 @@ function App() {
         />
 
         <ParallaxLayer 
-          offset={0}
+          offset={2.5}
           speed={1}
           factor={2}
+          style={{
+            backgroundColor: 'white'
+          }}
         />
 
+        {/* sticky layers */}
+        <ParallaxLayer 
+          sticky={{ start: 1.5, end: 5}} 
+          speed={-0.5}
+          className = 'skillBox'>
+          <h3>Skills</h3>
+        </ParallaxLayer>
+
+        <ParallaxLayer 
+          sticky={{ start: 2.5, end: 5}} 
+          speed={-0.5}
+          className = 'toolBox'>
+          <h3>Tool Box</h3>
+        </ParallaxLayer>
+
+        <ParallaxLayer 
+          offset={2.5}
+          speed={1}
+          factor={1.5}
+          style={{
+            backgroundColor: 'white'
+          }}
+        />
+
+
+
+        {/* timeline */}
         <ParallaxLayer offset={0}>
-      
             <h1>Matteo Larrode</h1>
             <h1>Welcome to my website</h1>
-            <h3>start scrolling!</h3>
-            <img src={profileImage} alt="Profile" width="400" height= "auto" />
-
+            <h2>start scrolling!</h2>
+            <img src={profileImage} alt="Profile" width="300" height= "auto" />
         </ParallaxLayer>
           
         <ParallaxLayer offset={1}>
@@ -54,12 +81,12 @@ function App() {
           <h1>Undergrad: First Year</h1>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={3}>
-          <h1>Undergrad: Second Year</h1>
+        <ParallaxLayer offset={3} style={{backgroundColor:'white'}}>
+          <h1 className='blackHeader'> Undergrad: Second Year</h1>
         </ParallaxLayer>
 
-        <ParallaxLayer offset={4}>
-          <h1>Undergrad: Year Abroad</h1>
+        <ParallaxLayer offset={4} style={{backgroundColor:'white'}}>
+          <h1 className='blackHeader'> Undergrad: Year Abroad</h1>
         </ParallaxLayer>
         
       </Parallax>
